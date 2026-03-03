5273b68ebd21b66b0f18bf408f04707398ae6cfb
ulpianarexhepi <ulpiana.rexhepi@ibu.edu.mk>
2026-03-03 10:23:29 +0100

    assignment 2 finished

--- github-education-c-exercises/04-uppercase/uppercase.c
+++ github-education-c-exercises/04-uppercase/uppercase.c
@@ -9,6 +9,9 @@ int main(void)
     printf("After: ");
 
     // TODO: loop through the string and print toupper(...) for each char
-
+for (int i = 0; i < strlen(s); i++)
+{
+    printf("%c", toupper(s[i]));
+}
     printf("\n");
 }
--- github-education-c-exercises/05-argv-greet/greet.c
+++ github-education-c-exercises/05-argv-greet/greet.c
@@ -6,5 +6,12 @@ int main(int argc, string argv[])
     // TODO:
     // if argc == 2 print hello, argv[1]
     // else print hello, world
+    if (argc == 2)
+    {
+        printf("Hello, %s\n", argv[1]);
+    } else
+    {
+        printf("hello, world\n");
+    }
     return 0;
 }
